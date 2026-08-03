f8196d75f4fd1f7b04f44e27be64e986917efc0d
mohammed ayan <mohammedayan71636@gmail.com>
2026-08-03 16:31:57 +0530

    object complete  read

---
+++ 02) Object/app.js
@@ -0,0 +1,217 @@
+/*
+var obj1 = new Object();
+obj1.name = "ameer";
+obj1.age = 22;
+obj1.city = "Sojat City";
+console.log(obj1);
+*/
+
+
+
+
+
+
+//----------------------------Constructor Function------------------------
+/*
+function Student(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+
+
+//----------------------------Object create krnaaa-------------------------
+let s1 = new Student("ameer",22);
+let s2 = new Student("sahil",25);
+
+
+
+//-----------------------------Output--------------------------------------
+console.log(s1);
+console.log(s2);
+
+delete s1.age;
+console.log(s1.name.toUpperCase());
+console.log(s2.name.toUpperCase());
+*/
+
+
+
+
+
+
+
+
+
+// --------------------------------changes key---------------------------------------
+/*
+let obj1 = {
+    name : "ameer",
+    age : 22,
+    city : "Sojat City",
+}
+
+// console.log(obj1);
+
+obj1.fullname = obj1.name;
+delete obj1.name;
+console.log(obj1);
+*/
+
+
+
+
+
+
+
+
+
+
+// -------------------------------update value using key---------------------------------------
+/*
+let user = {
+  name: 'Ayan',
+  age: 31
+};
+user.age = 32;
+console.log(user);
+*/
+
+
+
+
+
+
+
+
+// ----------------------------------------------remove key------------------------------------
+/*
+let obj = {
+    name: "ameer",
+    age: 22,
+    city: "Sojat City",
+}
+delete obj.city;
+console.log(obj);
+*/
+
+
+
+
+
+
+
+
+
+// --------------------------------------remove value-----------------------------------------
+/*
+let obj = {
+    name: "ameer",
+    age: 22,
+    city: "Sojat City",
+}
+obj.city = undefined;
+obj.city = "";
+console.log(obj);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------nested objects-------------------------------------
+/*
+let user = {
+  name: 'Ayan',
+  age: 31,
+  address: {
+    city: 'Jodhpur',
+    state: 'Rajasthan',
+    pin: 342001
+  }
+};
+
+console.log(user);
+console.log(user.address);
+*/
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------object : array-------------------------------------
+/*
+let obj = {
+    name: "ameer",
+    age: 22,
+    city: "Sojat City",
+    hobbies : ["cricket", "football", "coding"]
+}
+console.log(obj);
+console.log(obj.hobbies);
+console.log(obj.hobbies[0]);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
